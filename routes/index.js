19f5a812539a185a527ea8e339ad77ccda5fee1e
Pedro Louro <pedrolouro93@gmail.com>
2023-04-22 16:59:38 +0100

    solved lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res) => {
+  const getMovies = await Movie.find()
+  res.render('movies', {getMovies})
+})
+
+router.get('/movie/:id', async (req, res) => {
+  const oneMovie = await Movie.findById(req.params.id)
+  console.log(oneMovie)
+  res.render('movie-details', oneMovie)
+})
 
 module.exports = router;
